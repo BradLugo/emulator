a990a9464fea49578075d9ac3baad06bda50facd
Brad Lugo <cblugo95@yahoo.com>
2018-03-27 20:39:40 -0500

    Add logging functionality
    Things to note:
    - You need to free the string returned from generate_log_message
    - The generate_log_message takes a few more parameters than it needs to
    so it's easier to write unit tests for

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "emulator.h"
+#include "logger.h"
 
 int main() {
     printf("Hello, World!\n");
     int c = 0;
     printf("Before :: %d %d\n", half_adder(0, 1, &c), c);
     printf("After :: %d\n", c);
-    return 0;
+
+    return EXIT_SUCCESS;
 }
--- test/tests.c
+++ test/tests.c
@@ -1,5 +1,10 @@
+#include <ntsid.h>
+#include <time.h>
+#include <memory.h>
+#include <stdlib.h>
 #include "unity.h"
 #include "emulator.h"
+#include "logger.h"
 
 void setUp(void)
 {
@@ -63,9 +68,45 @@ void test_full_adder(void)
     TEST_ASSERT_EQUAL(1, c);
 }
 
+void test_logging(void)
+{
+    char *message = "test123";
+    char compare[1024];
+    char *log_message;
+    time_t now;
+    time(&now);
+
+    sprintf(compare, "%s [%s]: %s", ctime(&now), "DEBUG", message);
+    log_message = generate_log_message(DEBUG_LOG_LEVEL, message, now, 1024);
+    TEST_ASSERT(strcmp(compare, log_message) == 0);
+    free(log_message);
+
+    sprintf(compare, "%s [%s]: %s", ctime(&now), "INFO", message);
+    log_message = generate_log_message(INFO_LOG_LEVEL, message, now, 1024);
+    TEST_ASSERT(strcmp(compare, log_message) == 0);
+    free(log_message);
+
+    sprintf(compare, "%s [%s]: %s", ctime(&now), "WARN", message);
+    log_message = generate_log_message(WARN_LOG_LEVEL, message, now, 1024);
+    TEST_ASSERT(strcmp(compare, log_message) == 0);
+    free(log_message);
+
+    sprintf(compare, "%s [%s]: %s", ctime(&now), "ERROR", message);
+    log_message = generate_log_message(ERROR_LOG_LEVEL, message, now, 1024);
+    TEST_ASSERT(strcmp(compare, log_message) == 0);
+    free(log_message);
+
+    sprintf(compare, "%s [%s]: %s", ctime(&now), "FATAL", message);
+    log_message = generate_log_message(FATAL_LOG_LEVEL, message, now, 1024);
+    TEST_ASSERT(strcmp(compare, log_message) == 0);
+    free(log_message);
+}
+
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_half_adder);
     RUN_TEST(test_full_adder);
+    RUN_TEST(test_logging);
     return UNITY_END();
 }
