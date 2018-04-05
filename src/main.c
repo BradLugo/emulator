b4946a248b655b86002ec6a80a3921d917ac5a9d
Aldo Peralta <aldo.peralta00@gmail.com>
2018-04-05 12:55:08 -0500

    Adding multiplexer component
    modified .gitignore to ignore run.bash

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "emulator.h"
+#include "multiplexer.h"
 
 int main() {
     printf("Hello, World!\n");
     int c = 0;
+    int s0 = 0;
     printf("Before :: %d %d\n", half_adder(0, 1, &c), c);
     printf("After :: %d\n", c);
+    printf("mux 2to1 0 1 switch false => %d\n", mux_2to1(0, 1, s0));
     return 0;
 }
