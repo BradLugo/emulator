05de66ef08befb5c5647f7c93df129ccc6ac6f51
JoeyDrew <joey@joeydrew.com>
2018-04-15 11:01:59 -0700

    added components.c with header and tests

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 int main() {
     printf("Hello, World!\n");
     int c = 0;
+	int a = 100;
+	int b = 6;
     printf("Before :: %d %d\n", half_adder(0, 1, &c), c);
     printf("After :: %d\n", c);
     return 0;
--- test/tests.c
+++ test/tests.c
@@ -1,5 +1,5 @@
 #include "unity.h"
-#include "emulator.h"
+#include "components.h"
 
 void setUp(void)
 {
@@ -9,63 +9,41 @@ void tearDown(void)
 {
 }
 
-void test_half_adder(void)
+void test_and(void)
 {
-    int c = 0;
-    TEST_ASSERT_EQUAL(0, half_adder(0, 0, &c));
-    TEST_ASSERT_EQUAL(0, c);
-
-    c = 0;
-    TEST_ASSERT_EQUAL(1, half_adder(0, 1, &c));
-    TEST_ASSERT_EQUAL(0, c);
-
-    c = 0;
-    TEST_ASSERT_EQUAL(1, half_adder(1, 0, &c));
-    TEST_ASSERT_EQUAL(0, c);
-
-    c = 0;
-    TEST_ASSERT_EQUAL(0, half_adder(1, 1, &c));
-    TEST_ASSERT_EQUAL(1, c);
+	TEST_ASSERT_EQUAL(1, and(1,1));
+	TEST_ASSERT_EQUAL(0, and(0,0));
+	TEST_ASSERT_EQUAL(0, and(1,0));
+	TEST_ASSERT_EQUAL(0, and(0,1));
 }
 
-void test_full_adder(void)
+void test_or(void)
 {
-    int c = 0;
-    TEST_ASSERT_EQUAL(0, full_adder(0, 0, 0, &c));
-    TEST_ASSERT_EQUAL(0, c);
+	TEST_ASSERT_EQUAL(1, or(1,1));
+	TEST_ASSERT_EQUAL(0, or(0,0));
+	TEST_ASSERT_EQUAL(1, or(1,0));
+	TEST_ASSERT_EQUAL(1, or(0,1));
+}
 
-    c = 0;
-    TEST_ASSERT_EQUAL(1, full_adder(0, 1, 0, &c));
-    TEST_ASSERT_EQUAL(0, c);
+void test_not(void)
+{
+	TEST_ASSERT_EQUAL(0, not(1));
+	TEST_ASSERT_EQUAL(1, not(0));
+}
 
-    c = 0;
-    TEST_ASSERT_EQUAL(1, full_adder(1, 0, 0, &c));
-    TEST_ASSERT_EQUAL(0, c);
-
-    c = 0;
-    TEST_ASSERT_EQUAL(0, full_adder(1, 1, 0, &c));
-    TEST_ASSERT_EQUAL(1, c);
-
-    c = 0;
-    TEST_ASSERT_EQUAL(1, full_adder(0, 0, 1, &c));
-    TEST_ASSERT_EQUAL(0, c);
-
-    c = 0;
-    TEST_ASSERT_EQUAL(0, full_adder(0, 1, 1, &c));
-    TEST_ASSERT_EQUAL(1, c);
-
-    c = 0;
-    TEST_ASSERT_EQUAL(0, full_adder(1, 0, 1, &c));
-    TEST_ASSERT_EQUAL(1, c);
-
-    c = 0;
-    TEST_ASSERT_EQUAL(1, full_adder(1, 1, 1, &c));
-    TEST_ASSERT_EQUAL(1, c);
+void test_xor(void)
+{
+	TEST_ASSERT_EQUAL(0, xor(1,1));
+	TEST_ASSERT_EQUAL(0, xor(0,0));
+	TEST_ASSERT_EQUAL(1, xor(1,0));
+	TEST_ASSERT_EQUAL(1, xor(0,1));
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_half_adder);
-    RUN_TEST(test_full_adder);
+    RUN_TEST(test_and);
+    RUN_TEST(test_or);
+    RUN_TEST(test_not);
+    RUN_TEST(test_xor);
     return UNITY_END();
 }
