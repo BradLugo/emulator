602696f92e914332e64fb61f77ab58080642f813
Fernando.Picazo <Fernando.Picazo@outlook.com>
2018-04-21 23:29:35 -0500

    Created a skeleton for the decoder as well as function for fetching instructions

--- src/emulator.h
+++ src/emulator.h
@@ -4,8 +4,18 @@
 
 #ifndef EMULATOR_EMULATOR_H
 #define EMULATOR_EMULATOR_H
+//IINCLUDES
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//Global Variables
+unsigned char* MEMORY[16];
 
 int half_adder(int a, int b, int *cout);
 int full_adder(int a, int b, int cin, int *cout);
+
+int init_emulator(char* filename);
+unsigned char get_byte(unsigned char **ptr);
 
 #endif //EMULATOR_EMULATOR_H
