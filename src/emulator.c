2745cdcc02028477637677671aa456f408126c5b
Fernando.Picazo <Fernando.Picazo@outlook.com>
2018-04-21 19:22:21 -0500

    Created an initialization function which allots memory for emulator.

--- src/emulator.c
+++ src/emulator.c
@@ -16,3 +16,39 @@ int full_adder(int a, int b, int cin, int *cout)
     *cout = (xor & cin) | (a & b);
     return xor ^ cin;
 }
+
+int init_emulator(char *filename)
+{
+    int i, j;
+    FILE* file;
+    //allocate space for memory
+    for (i = 0; i < 16; i++)
+    {
+        if(i==14)continue;//IO is not emulated in this case
+        MEMORY[i] = (unsigned char *)malloc(0x1000*sizeof(unsigned char));
+        if(MEMORY[i] == NULL)
+        {
+            printf("Error allocating memory at component %d", i);
+            return -1;
+        }
+    }
+
+    if((file = fopen(filename, "r")) == NULL){
+        printf("Unable to open file");
+        return -1;
+    }
+
+    //read file instructions into EPROM Location
+    j = 0;
+    unsigned char buff;
+    while(fscanf(file, "%02X",(unsigned int*) &buff) != EOF && (j < 0x1000)){
+        memcpy(&MEMORY[15][j], &buff, sizeof(unsigned char));
+        j++;
+    }
+    MEMORY[15][j] = '\0';
+
+    //Set the state of any structures here
+    return 0;
+}
+
+
--- src/emulator.h
+++ src/emulator.h
@@ -4,8 +4,17 @@
 
 #ifndef EMULATOR_EMULATOR_H
 #define EMULATOR_EMULATOR_H
+//IINCLUDES
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//Global Variables
+unsigned char* MEMORY[16];
 
 int half_adder(int a, int b, int *cout);
 int full_adder(int a, int b, int cin, int *cout);
+
+int init_emulator(char* filename);
 
 #endif //EMULATOR_EMULATOR_H
