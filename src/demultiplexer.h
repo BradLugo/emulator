aecd03410ed729c0f372146ec84403dbfe03c9fa
AldoPeralta <aldo.peralta00@gmail.com>
2018-04-23 19:13:39 -0500

    Update demultiplexer.h

--- src/demultiplexer.h
+++ src/demultiplexer.h
@@ -1,5 +1,5 @@
 #ifndef DEMULTIPLEXER_DEMULTIPLEXER_H
 #define DEMULTIPLEXER_DEMULTIPLEXER_H
-void demux_1to4(int input, int sel1, int sel0, int *A, int *B, int *C, int *D);
+void demux_2to4(int input, int sel1, int sel0, int *A, int *B, int *C, int *D);
 
 #endif
